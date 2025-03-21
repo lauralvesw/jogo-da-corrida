a7970a046e0a458cd0484d5cb5e28fda2e150092
lauralvesw <160248344+lauralvesw@users.noreply.github.com>
2025-03-21 07:54:38 -0300

    s

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  let xJogador1 = 0;
+  let xJogador2 = 0;
+  
+  function draw() {
+    if (focused == true) {
+      background("#D2EBB5");
+    } else {
+      background("rgb(238,178,178)");
+    }
+  
+    textSize(40);
+    text("🌺", xJogador1, 100);
+    text("💘", xJogador2, 300);
+    rect(350, 0, 10, 400);
+    if (xJogador1 > 350) {
+      text("Jogador 1 venceu!", 50, 200);
+      noLoop();
+    }
+    if (xJogador2 > 350) {
+      text("Jogador 2 venceu!", 50, 200);
+      noLoop();
+    }
+  }
+  
+  function keyReleased() {
+    if (key == "w") {
+      xJogador1 += random(20);
+    }
+    if (key == "l") {
+      xJogador2 += random(20);
+    }
+  }
+  
